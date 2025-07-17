3a5b3965ab487d15fa127e507f17435e9eb1bf62
kavendra2004 <kavendraahirwar2004@gmail.com>
2025-07-17 13:40:49 +0530

    fix: added proper CORS config

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,7 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+app.options('*', cors()); // Handle preflight
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
